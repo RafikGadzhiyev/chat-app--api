af9f32e61dfd88ff4cf50ae4014b19fb3ce2ae05
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-12-29 16:57:44 +0500

    feat(route): add session checker route

--- src/api/auth/auth.route.js
+++ src/api/auth/auth.route.js
@@ -4,6 +4,12 @@ const controller = require('./auth.controller');
 
 const router = express.Router();
 
+// TODO: Do we need actually need that If we will connect auth middleware?
+router.get(
+  '/session',
+  controller.session,
+);
+
 router.post(
   '/sign-in',
   controller.signIn,
