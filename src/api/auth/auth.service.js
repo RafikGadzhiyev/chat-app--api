a7027d9fbf37f59f6e225a17ccf00404fd9e276c
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-12-29 14:34:43 +0500

    feat(auth): add token JWT token validation service

--- src/api/auth/auth.service.js
+++ src/api/auth/auth.service.js
@@ -1,4 +1,6 @@
 const User = require('./../../models/User.model');
+const jwt = require('jsonwebtoken');
+const enumHelper = require('../../helpers/enum.helper');
 
 async function getUser(searchQuery) {
   const user = await User
@@ -11,6 +13,29 @@ async function getUser(searchQuery) {
   return user;
 }
 
+function validateToken(token, tokenType) {
+  try {
+    let tokenSecret = process.env.TOKEN_SECRET;
+
+    if (tokenType === enumHelper.TOKEN_TYPES.ACCESS_TOKEN) {
+      tokenSecret = process.env.ACCESS_TOKEN_SECRET;
+    } else if (tokenType === enumHelper.TOKEN_TYPES.REFRESH_TOKEN) {
+      tokenSecret = process.env.REFRESH_TOKEN_SECRET;
+    }
+
+    return jwt.verify(
+      token,
+      tokenSecret,
+    );
+  } catch (err) {
+    console.error(err);
+
+    return null;
+  }
+}
+
 module.exports = {
   getUser,
+
+  validateToken,
 };
